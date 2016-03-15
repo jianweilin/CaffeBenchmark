118d6554089e8949ba25b8fb29af70da6228aba0
Tom Runia <runia@uva.nl>
2016-03-15 13:33:27 +0100

    Changes before commit...

--- classification.py
+++ classification.py
@@ -5,6 +5,8 @@ import glob
 import datetime
 
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import caffe
 import cv2
 
@@ -12,13 +14,18 @@ import cv2
 caffe_root = "/home/trunia1/dev/lib/caffe/"
 
 
-def load_network(model_def, model_weights, batch_size, input_dim):
+def load_network(model_def, model_weights, batch_size, input_dim, print_network_layout=False):
 
     caffe.set_mode_gpu()
 
     # Use TEST mode, e.g. don't perform dropout
     net = caffe.Net(model_def, model_weights, caffe.TEST)
     net.blobs['data'].reshape(batch_size, input_dim[0], input_dim[1], input_dim[2])
+
+    if print_network_layout:
+        print("Network Layout:")
+        for layer_name, blob in net.blobs.iteritems():
+            print("  layer_name %s \t %s" % (layer_name, str(blob.data.shape)))
 
     return net
 
@@ -100,6 +107,32 @@ def predict_images(net, image_dir):
     cv2.destroyAllWindows()
 
 
+def visualize_square(data):
+
+    # normalize data for display
+    data = (data - data.min()) / (data.max() - data.min())
+
+    # force the number of filters to be square
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = (((0, n ** 2 - data.shape[0]),
+               (0, 1), (0, 1))                 # add some space between filters
+               + ((0, 0),) * (data.ndim - 3))  # don't pad the last dimension (if there is one)
+    data = np.pad(data, padding, mode='constant', constant_values=1)  # pad with ones (white)
+
+    # tile the filters into an image
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+
+    plt.imshow(data)
+    plt.axis('off')
+    plt.show()
+
+
+def visualize_conv_layer(net, layer_name):
+    print("Visualizing filters for layer '%s'..." % layer_name)
+    filters = net.params[layer_name][0].data
+    visualize_square(filters.transpose(0, 2, 3, 1))
+
 
 if __name__ == '__main__':
 
@@ -112,25 +145,28 @@ if __name__ == '__main__':
     #model_weights = "/home/trunia1/data/CaffeModels/bvlc_googlenet.caffemodel"
 
     # VGG ILSVR (16 layers)
-    #model_def =     "/home/trunia1/data/CaffeModels/VGG_ILSVRC_16_layers_deploy.prototxt"
-    #model_weights = "/home/trunia1/data/CaffeModels/VGG_ILSVRC_16_layers.caffemodel"
+    model_def =     "/home/trunia1/data/CaffeModels/VGG_ILSVRC_16_layers_deploy.prototxt"
+    model_weights = "/home/trunia1/data/CaffeModels/VGG_ILSVRC_16_layers.caffemodel"
 
     # VGG ILSVR (19 layers)
     #model_def =     "/home/trunia1/data/CaffeModels/VGG_ILSVRC_19_layers_deploy.prototxt"
     #model_weights = "/home/trunia1/data/CaffeModels/VGG_ILSVRC_19_layers.caffemodel"
 
     # Microsoft ResNets
-    model_def =     "/home/trunia1/data/CaffeModels/deep-residual-networks/ResNet-50-deploy.prototxt"
-    model_weights = "/home/trunia1/data/CaffeModels/deep-residual-networks/ResNet-50-model.caffemodel"
+    model_def =     "/home/trunia1/data/CaffeModels/deep-residual-networks/ResNet-152-deploy.prototxt"
+    model_weights = "/home/trunia1/data/CaffeModels/deep-residual-networks/ResNet-152-model.caffemodel"
 
-    image_dir =     "/home/trunia1/data/MS-COCO/val2014/"
+    #image_dir =     "/home/trunia1/data/MS-COCO/val2014/"
+    image_dir = "/home/trunia1/data/test-cat/"
 
     batch_size = 1
     #input_dim  = (3, 227, 227)     # AlexNet
-    input_dim  = (3, 224, 224)     # GoogLeNet, VGG
+    input_dim  = (3, 224, 224)      # GoogLeNet, VGG, MS ResNet
 
     # Load Caffe network from definition+weights
-    network = load_network(model_def, model_weights, batch_size, input_dim)
+    network = load_network(model_def, model_weights, batch_size, input_dim, False)
+
+    #visualize_conv_layer(network, "conv3")
 
     # Predict all images in the directory one-by-one
     predict_images(network, image_dir)
